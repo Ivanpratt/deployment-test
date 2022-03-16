2c4556b4b4f854db9519887ba77fed05071e55a9
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-16 12:37:22 -0600

    done 2

--- server/server.js
+++ server/server.js
@@ -7,15 +7,15 @@ app.use(cors());
 app.use(express.json());
 
 //middleware
-app.use(exress.static(path.join(__dirname, "../public")))
+// app.use(exress.static(path.join(__dirname, "../public")))
+// app.use("/styles"), express.static(path.join)
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
-
-// app.get("/styles", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/styles.css"));
-// });
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/styles.css"));
+});
 
 const port = process.env.PORT || 4005;
 
