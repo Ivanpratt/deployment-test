230ce6e9c1c325bda5e68c64e28b08b31d818c6c
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-16 12:05:47 -0600

    done

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
-app.use(express.json())
-
+app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"))
-})
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
 
-const port = process.env.port || 4005;
+const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
-    console.log(`we vibing on ${port}`)
-})
+    console.log(`We vibin on port ${port}`);
+});
